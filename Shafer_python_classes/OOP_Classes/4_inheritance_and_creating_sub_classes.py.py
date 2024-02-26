5758f09601b9089d38c0a46a88671787b2bd99f0
leopinzon75 <112821659+leopinzon75@users.noreply.github.com>
2024-02-25 21:28:49 -0800

    Update 4_inheritance_and_creating_sub_classes.py.py

--- Shafer_python_classes/OOP_Classes/4_inheritance_and_creating_sub_classes.py.py
+++ Shafer_python_classes/OOP_Classes/4_inheritance_and_creating_sub_classes.py.py
@@ -1,58 +1,149 @@
-'''inheritance_and_creating_sub_classes'''
-
-
+ 
 class Employee:
-    
+
     raise_amt = 1.04
 
     def __init__(self, first, last, pay):
-            
         self.first = first
         self.last = last
+        self.email = first + '.' + last + '@email.com'
         self.pay = pay
-        self.email = first + '_' + last + '@company.com'
 
     def fullname(self):
-        return'{} {}'.format(self.first, self.last)
+        return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
+
+
+
+# class Developer(Employee):
+#     raise_amt = 1.10 # We can make this changes in the subclass without
+                     #havig to worry of breaking the parents class
+
+# dev_1 = Employee('Corey', 'Schafer', 50000)
+# dev_2 = Developer('Test', 'Employee', 60000)
+#
+#
+# print(dev_1.email)
+# print(dev_2.email)
+
+# print(dev_1.pay)# 50000
+# dev_1.apply_raise()
+# print(dev_1.pay)# 52000  # We can make this changes in the subclass without
+                         #havig to worry of breaking the parents class
+
+
+# To add more attributes internally into the subclass, for example programmin 
+#languaje, we will do this:
+
 class Developer(Employee):
-    #raise_amt = 1.10
 
     def __init__(self, first, last, pay, prog_lang):
         super().__init__(first, last, pay)
-        self.prog_lang = prog_lang 
+        self.prog_lang = prog_lang
 
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java')
+
+# print(dev_1.email)
+# print(dev_1.prog_lang)
+
+
+# Instead of super you can also do this: Employee.__init__(self, first, last, pay)
+
+
+
+# Now let's create another subclass called manager with new functions:
+# In this class we are going to provide a list of employees that the manager supervises
+# at the init method and equal to none
 
 class Manager(Employee):
-    
-    def __init__(self, first, last, pay, employees=None):
+
+    def __init__(self, first, last, pay, employees=None):# Do not pass a mutable data type as default argument
         super().__init__(first, last, pay)
         if employees is None:
-             self.employees = []
+            self.employees = []
         else:
-             self.employees = employees
+            self.employees = employees
 
-    def add_emp(self, emp):
-        if emp not in self.employees:
-             self.employees.append(emp)
+# Methods to add and remove employees that our manager supervises:
 
-    def remove_emp(self, emp):
-        if emp in self.employees:
-             self.employees.remove(emp)
+    def add_dev(self, dev):
+        if dev not in self.employees:
+            self.employees.append(dev)# Attention of 3 words together with dot notation = 2 dots in the statement
 
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
-           
+    def remove_dev(self, dev):
+        if dev in self.employees:
+            self.employees.remove(dev)
+  
 
-dev_1 = Developer("corey","Schafer", 50000, 'Python')
-dev_2 = Developer("test","user", 60000, 'Java') 
 
-mgr_1 = Manager('Sue', 'Smith', 90000,[dev_2])
+# To print out all of the employees that the manager supervises
+            
+    def print_devs(self):
+        for dev in self.employees:
+            print('-->', dev.fullname())
 
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])# Here is where we find the list created at the init method
+
+# print(mgr_1.email)# Sue.Smith@email.com
+mgr_1.print_devs()# --> Corey Schafer 
+mgr_1.add_dev(dev_2)# --> Test Employee
+mgr_1.print_devs()#---> Corey Schafer
+# mgr_1.remove_emp(dev_2)# user1s-MacBook-Pro:Shafer_Python_Classes admin$ /usr/local/bin/python3 /Users/admin/Desktop/Shafer_Python_Classes/4-Inheritance/oop.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(help(Developer))
+#  |      Employee
+#  |  
+#  |  Method resolution order:
+#  |      Developer
+#  |      Employee
+#  |      builtins.object
+#  |   
+#  |  Methods inherited from Employee:
+#  |  
+#  |  __init__(self, first, last, pay)
+#  |      Initialize self.  See help(type(self)) for accurate signature.
+#  |  
+#  |  apply_raise(self)
+#  |  
+#  |  fullname(self)
+#  |  
+#  |  ----------------------------------------------------------------------
+#  |  Data descriptors inherited from Employee:
+#  |  
+#  |  __dict__
+#  |      dictionary for instance variables (if defined)
+#  |  
+#  |  __weakref__
+#  |      list of weak references to the object (if defined)
+#  |  
+#  |  ----------------------------------------------------------------------
+#  |  Data and other attributes inherited from Employee:
+#  |  
+#  |  raise_amt = 1.04
+
+
+
+
+
+# print(dev_1.raise_amt)
+# dev_1.apply_raise()
+# print(dev_1.raise_amt)
 
 
 mgr_1.print_emps()
